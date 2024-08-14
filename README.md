d7e2edafa8ad750ed4142b7107cb81780141545f
knvmrt <knvmrt@gmail.com>
2024-08-14 14:15:05 +0400

    Updated Styles and added error messages.

--- README.md
+++ README.md
@@ -1,13 +1,37 @@
-# Hello, this course registration is the system.
+# ✋Hello, this course registration is the system.
 
-# 💻Project tech:
+## 💻Project tech:
 
+![JavaScript](https://img.shields.io/badge/java%20script-%23223300.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
 ![Next.js](https://img.shields.io/badge/next.js-%23101010.svg?style=for-the-badge&logo=next.js&logoColor=%23ffffff)
 ![Tailwindcss](https://img.shields.io/badge/tailwindcss-%23202366.svg?style=for-the-badge&logo=tailwindcss&logoColor=white)
 ![CSS](https://img.shields.io/badge/css-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
 ![SUPABASE](https://img.shields.io/badge/supabase-%233DCF8E.svg?style=for-the-badge&logo=supabase&logoColor=white)
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## To mount Supabase, first create the `.env.local` file in the main directory.
+
+### And the supabase API URL and ANON KEY should be added to the content of the file.
+
+```env
+NEXT_PUBLIC_SUPABASE_URL=
+NEXT_PUBLIC_SUPABASE_ANON_KEY=
+```
+
+## To create SQL tables:
+
+```sql
+CREATE TABLE users (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    birth_date DATE NOT NULL,
+    category TEXT NOT NULL,
+    time_range TEXT NOT NULL,
+    created_at DATE DEFAULT CURRENT_DATE
+);
+```
+
+## To start the project:
 
 ```bash
 npm run dev
@@ -18,6 +42,8 @@ pnpm dev
 # or
 bun dev
 ```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
 # 🚀 Project GitHub:
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,11 +8,26 @@ const Home = () => {
   const [category, setCategory] = useState('');
   const [timeRange, setTimeRange] = useState('');
 
+  const [errors, setErrors] = useState({});
+
   const categories = ['Graphic Design', 'UI/UX Design', 'Front-End', 'Back-End', 'Full-Stack', 'MERN-Stack', 'Ingils dili', 'Rus dili', 'MS-Office']; // Courses
   const timeRanges = ['Late Morning', 'Late Afternoon', 'Same']; // Times
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const newErrors = {};
+
+    if (!firstName) newErrors.firstName = 'First Name is required';
+    if (!lastName) newErrors.lastName = 'Last Name is required';
+    if (!birthDate) newErrors.birthDate = 'Birth Date is required';
+    if (!category) newErrors.category = 'Category is required';
+    if (!timeRange) newErrors.timeRange = 'Time Range is required';
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+      return;
+    }
 
     const { data, error } = await supabase
       .from('users')
@@ -28,78 +43,69 @@ const Home = () => {
       setBirthDate('');
       setCategory('');
       setTimeRange('');
+      setErrors({}); // Clear error messages
     }
   };
 
-
-  // if (error) {
-  //   console.error('Error inserting data:', error);
-  //   // Hata mesajını burada görebilirsiniz.
-  // }
-
-
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
-        <h1 className="text-2xl font-bold mb-6 text-center">User Form</h1>
-        <form onSubmit={handleSubmit} className="space-y-6">
+    <div className="min-h-screen flex items-center justify-center bg-gray-800">
+      <div className="bg-gray-900 p-8 rounded-lg max-w-md w-full shadow-[0_30px_50px_-10px_#0106118c]">
+        <h1 className="text-3xl font-bold mb-6 text-center text-white">User Form</h1>
+        <form onSubmit={handleSubmit} className="space-y-5">
           <div>
-            <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">First Name:</label>
+            <label htmlFor="firstName" className="block text-sm font-medium text-gray-300">First Name:</label>
             <input
               type="text"
               id="firstName"
               value={firstName}
               onChange={(e) => setFirstName(e.target.value)}
-              required
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
+              placeholder='First Name' />
+            {errors.firstName && <p className="text-red-500 text-sm mt-1">{errors.firstName}</p>}
           </div>
           <div>
-            <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">Last Name:</label>
+            <label htmlFor="lastName" className="block text-sm font-medium text-gray-300">Last Name:</label>
             <input
               type="text"
               id="lastName"
               value={lastName}
               onChange={(e) => setLastName(e.target.value)}
-              required
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
+              placeholder='Last Name' />
+            {errors.lastName && <p className="text-red-500 text-sm mt-1">{errors.lastName}</p>}
           </div>
           <div>
-            <label htmlFor="birthDate" className="block text-sm font-medium text-gray-700">Birth Date:</label>
+            <label htmlFor="birthDate" className="block text-sm font-medium text-gray-300">Birth Date:</label>
             <input
               type="date"
               id="birthDate"
               value={birthDate}
               onChange={(e) => setBirthDate(e.target.value)}
-              required
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
+            {errors.birthDate && <p className="text-red-500 text-sm mt-1">{errors.birthDate}</p>}
           </div>
-          {/*  Category */}
           <div>
-            <label htmlFor="category" className="block text-sm font-medium text-gray-700">Category:</label>
+            <label htmlFor="category" className="block text-sm font-medium text-gray-300">Courses:</label>
             <select
               id="category"
               value={category}
               onChange={(e) => setCategory(e.target.value)}
-              required
               className="mt-1 block w-full px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             >
-              <option value="" disabled>Select a category</option>
+              <option value="" disabled>Select a course</option>
               {categories.map((cat) => (
                 <option key={cat} value={cat}>{cat}</option>
               ))}
             </select>
+            {errors.category && <p className="text-red-500 text-sm mt-1">{errors.category}</p>}
           </div>
-          {/* Select Time Range */}
           <div>
-            <label htmlFor="timeRange" className="block text-sm font-medium text-gray-700">Select Time Range:</label>
+            <label htmlFor="timeRange" className="block text-sm font-medium text-gray-300">Select Time Range:</label>
             <select
               id="timeRange"
               value={timeRange}
               onChange={(e) => setTimeRange(e.target.value)}
-              required
               className="mt-1 block w-full px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             >
               <option value="" disabled>Select Time Range</option>
@@ -107,10 +113,11 @@ const Home = () => {
                 <option key={time} value={time}>{time}</option>
               ))}
             </select>
+            {errors.timeRange && <p className="text-red-500 text-sm mt-1">{errors.timeRange}</p>}
           </div>
           <button
             type="submit"
-            className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="w-full bg-blue-700 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-950 outline-none"
           >
             Submit
           </button>
